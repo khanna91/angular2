1a334e15fe34682cf8a253675f3cc096ee721d10
Rahul Khanna <rahul.khanna@ofbusiness.com>
2017-03-15 14:01:44 +0530

    initial setup

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { FsassPage } from './app.po';
+import {FsassPage} from './app.po';
 
 describe('fsass App', () => {
-  let page: FsassPage;
+    let page: FsassPage;
 
-  beforeEach(() => {
-    page = new FsassPage();
-  });
+    beforeEach(() => {
+        page = new FsassPage();
+    });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('app works!');
+    });
 });
