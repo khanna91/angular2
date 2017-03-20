f2dde839cf76ce94294c331cc5d8928c5f8ca0e6
Rahul Khanna <rahul.khanna@ofbusiness.com>
2017-03-20 16:22:06 +0530

    adding docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import {Component} from '@angular/core';
 import {ViewEncapsulation} from '@angular/core';
 
@@ -5,8 +6,20 @@ import {ViewEncapsulation} from '@angular/core';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['app.component.scss'],
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
 })
 export class AppComponent {
-    title = 'app works!';
+    location: Location;
+    constructor(location: Location) {
+        this.location = location;
+    }
+
+    pathMatch(path) {
+        return (location.pathname === path);
+    }
+
+    pathExist(path) {
+        return location.pathname.includes(path);
+    }
 }
